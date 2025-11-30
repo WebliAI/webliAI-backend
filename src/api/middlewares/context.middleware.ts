cb4840448d8e02c659d53342bb1337a681a4901c
shivam-bhadani <shivambhadani123@gmail.com>
2025-12-01 04:17:46 +0530

    feat(core): add context middleware for traceId, requestId, and client info

---
+++ src/api/middlewares/context.middleware.ts
@@ -0,0 +1,38 @@
+import { Context, Next } from 'hono';
+import {
+  generateTraceId,
+  generateRequestId,
+  isValidTraceId,
+} from "../../utils/trace-id.util";
+import { reqCtx } from './../../constants/context';
+import { RequestContext } from '../../types';
+
+export const contextMiddleware = async (c: Context, next: Next) => {
+  const traceIdHeader = c.req.header("x-trace-id");
+
+  const traceId =
+    traceIdHeader && isValidTraceId(traceIdHeader)
+      ? traceIdHeader
+      : generateTraceId();
+  const requestId = generateRequestId();
+
+  const ip =
+    c.req.header("cf-connecting-ip") ||
+    c.req.header("x-forwarded-for")?.split(",")[0].trim() ||
+    c.req.header("x-real-ip") ||
+    // Miniflare local fallback
+    (c.req.raw as any)?.remoteAddress ||
+    "unknown";
+
+  const context: RequestContext = {
+    traceId: traceId,
+    requestId: requestId,
+    startTime: Date.now(),
+    userAgent: c.req.header('user-agent') || '',
+    ip
+  };
+
+  c.set(reqCtx, context);
+
+  await next();
+};
