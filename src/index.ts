ae73afa2ef5f541c3e753da4e73a022fa599154f
shivam-bhadani <shivambhadani123@gmail.com>
2025-12-01 04:20:34 +0530

    feat(server): set up Hono app with context and logging middleware

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,11 @@
 import { Hono } from 'hono'
+import { contextMiddleware } from './api/middlewares/context.middleware';
+import { loggingMiddleware } from './api/middlewares/logging.middleware';
 
 const app = new Hono()
+
+app.use('*', contextMiddleware);
+app.use("*", loggingMiddleware);
 
 app.get('/', (c) => {
   return c.text('Hello Hono!')
